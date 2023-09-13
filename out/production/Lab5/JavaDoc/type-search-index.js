3c2e7b5855f57195fa700f580b815b3fd10e5ffc
frontierofyourmind <115410394+frontierofyourmind@users.noreply.github.com>
2023-09-13 22:39:33 +0300

    Add files via upload

---
+++ out/production/Lab5/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"src","l":"CommandInterpreter"},{"p":"src","l":"Coordinates"},{"p":"src","l":"FuelType"},{"p":"src","l":"Main"},{"p":"src","l":"Vehicle"},{"p":"src","l":"VehicleType"}];updateSearchResults();
